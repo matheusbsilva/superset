c2f8826dbe02e66bd194a54ef9d96f4f2d8025a0
Matheus Batista <matheusbattista@hotmail.com>
2023-07-10 20:01:58 -0300

    Add public link to share menu

--- superset-frontend/src/dashboard/components/Header/HeaderActionsDropdown/index.jsx
+++ superset-frontend/src/dashboard/components/Header/HeaderActionsDropdown/index.jsx
@@ -327,6 +327,7 @@ class HeaderActionsDropdown extends React.PureComponent {
             <ShareMenuItems
               url={url}
               copyMenuItemTitle={t('Copy permalink to clipboard')}
+              copyPublicLinkTitle="Copy public link to clipboard"
               emailMenuItemTitle={t('Share permalink by email')}
               emailSubject={emailSubject}
               emailBody={emailBody}
